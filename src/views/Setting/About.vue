5d9c96f7ce320935fa11fb54c1e60a6b0ad59367
asadahimeka <smallstar9637@yahoo.co.jp>
2024-03-20 22:24:28 +0800

    chore: release v1.15.1

--- src/views/Setting/About.vue
+++ src/views/Setting/About.vue
@@ -3,7 +3,7 @@
     <top-bar id="top-bar-wrap" />
     <h3 class="af_title">{{ $t('about.title') }}</h3>
     <van-cell-group :title="$t('about.about_site')">
-      <van-cell center :title="$t('about.version')" clickable label="v1.15.0" />
+      <van-cell center :title="$t('about.version')" clickable label="v1.15.1" />
       <van-cell center :title="$t('about.disclaimer')" is-link :label="$t('tips.click_view')" to="/setting/about/disclaimer" />
       <van-cell
         center
